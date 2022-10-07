1d99412170c0feb1e8a77e4062490a090274c1d8
ballzing <1048315549@qq.com>
2022-10-07 21:56:36 +0800

    modified:   README.md 	modified:   package-lock.json 	modified:   package.json 	modified:   src/App.vue 	deleted:    src/components/HelloWorld.vue 	new file:   src/components/MyFooter.vue 	new file:   src/components/MyHeader.vue 	new file:   src/components/MyItem.vue 	new file:   src/components/MyList.vue

---
+++ src/components/MyFooter.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="todo-footer" v-show="todos.length">
+    <label>
+      <input type="checkbox" v-model="isAll" />
+    </label>
+    <span>
+      <span>已完成{{ doneTotal }}</span> / 全部{{ todos.length }}
+    </span>
+    <button class="btn btn-danger" @click="clearAll">清楚已完成任务</button>
+  </div>
+</template>
+
+<script scoped>
+export default {
+  name: "MyFooter",
+  props: ["todos", "checkAllTodo", "clearAllTodo"],
+  computed: {
+    doneTotal() {
+      const count = this.todos.reduce((pre, current) => {
+        return pre + (current.done ? 1 : 0);
+      }, 0);
+      return count;
+    },
+    isAll: {
+      get() {
+        return this.doneTotal === this.todos.length && this.todos.length > 0;
+      },
+      set(value) {
+        this.checkAllTodo(value);
+      },
+    },
+  },
+  methods: {
+    clearAll() {
+      this.clearAllTodo();
+    },
+  },
+};
+</script>
+
+<style>
+/*footer*/
+.todo-footer {
+  height: 40px;
+  line-height: 40px;
+  padding-left: 6px;
+  margin-top: 5px;
+}
+.todo-footer label {
+  display: inline-block;
+  margin-right: 20px;
+  cursor: pointer;
+}
+.todo-footer label input {
+  position: relative;
+  top: -1px;
+  vertical-align: middle;
+  margin-right: 5px;
+}
+.todo-footer button {
+  float: right;
+  margin-top: 5px;
+}
+</style>
